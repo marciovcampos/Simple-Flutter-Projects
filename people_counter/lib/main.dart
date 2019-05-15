7b5584721e6e5cb7883d67a4d08cc9c7d394a602
Marcio Vinicius <marcio.campos@outlook.com>
2019-05-14 23:44:51 -0300

    [people_counter] add app img background

--- people_counter/lib/main.dart
+++ people_counter/lib/main.dart
@@ -3,20 +3,57 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
       title: "People Counter",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text("People: 0",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)
-          ), //Text
-          Text("Come In",
-              style:
-              TextStyle(color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0)
-          ), //Text
-
-        ], //<Widget>
+          Image.asset(
+            "images/restaurant.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 50.0),
+              ),
+              Text("PEOPLE: 0",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold)), //Text
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 140.0),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text("Come In",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 30.0)), //Text
+            ], //<Widget>
+          )
+        ],
       ))); //MaterialApp
 }
