1252aab84057c4e48833fd746d098c8af1d062aa
Marcio Vinicius <marcio.campos@outlook.com>
2019-06-01 02:57:03 -0300

    [currency_converter] build interface

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -9,7 +9,11 @@ const request = "https://api.hgbrasil.com/finance?key=83f2af0f";
 void main() async{
   runApp(
       MaterialApp(
-        home: Home()
+        home: Home(),
+        theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white
+        )
       ));
 }
 
@@ -25,6 +29,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,8 +67,54 @@ class _HomeState extends State<Home> {
                     )
                 );
               }else{
-                return Container(
-                  color: Colors.green,
+
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Real",
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "R\$"
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber, fontSize: 25.0
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Dollar",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "US\$"
+                        ),
+                        style: TextStyle(
+                            color: Colors.amber, fontSize: 25.0
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Euro",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "€\$"
+                        ),
+                        style: TextStyle(
+                            color: Colors.amber, fontSize: 25.0
+                        ),
+                      ),
+
+                    ],
+                  )
                 );
               }
           }
